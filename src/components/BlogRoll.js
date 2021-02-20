6b08873157598ba45c88736919fee88c4e054b2b
Bryan <BryanIsAProfessional@gmail.com>
2021-02-20 15:32:23 -0800

    Initial rework to graphql structure

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -73,28 +73,29 @@ BlogRoll.propTypes = {
 export default () => (
   <StaticQuery
     query={graphql`
-      query BlogRollQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-        ) {
-          edges {
-            node {
-              excerpt(pruneLength: 400)
-              id
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                templateKey
-                date(formatString: "MMMM DD, YYYY")
-                featuredpost
-              }
+    query BlogRollQuery {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 400)
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              templateKey
+              date(formatString: "MMMM DD, YYYY")
+              featuredpost
+              
             }
           }
         }
       }
+    }
     `}
     render={(data, count) => <BlogRoll data={data} count={count} />}
   />
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -76,10 +76,8 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
         title
         description
-        tags
       }
     }
   }
